fe34b615b7bdb417f604ddc706594f95a6c5754e
samiksadhu <sadhusamik@gmail.com>
2022-01-01 18:28:47 +0530

    modulation features MI analysis

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,4 @@
-export train_cmd="queue.pl"
+export train_cmd="queue.pl -l 'hostname=!b03*'"
 export decode_cmd="queue.pl --mem 2G"
 export cuda_cmd="queue.pl --gpu 1 -l 'hostname=b1[12345678]*|c*'"
 export cuda_ccmd="queue.pl --gpu 1 -l 'hostname=c*'"
