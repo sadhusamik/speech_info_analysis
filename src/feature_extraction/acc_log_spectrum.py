d5cadb51c66ac4d5289059551f448710f4a040df
samiksadhu <sadhusamik@gmail.com>
2022-05-26 13:08:29 -0400

    average spectrum computation

--- src/feature_extraction/acc_log_spectrum.py
+++ src/feature_extraction/acc_log_spectrum.py
@@ -113,6 +113,7 @@ def compute_modulations(args):
                     acc_dct += dct_sum
                     acc_dst += dst_sum
                     count += cc
+                    print('%s:Extracted %d frames for file: %s' % (sys.argv[0], cc, uttid))
 
 
     pkl.dump({'count': count, 'acc_dct': acc_dct, 'acc_dst': acc_dst}, open(args.outfile, 'wb'))
