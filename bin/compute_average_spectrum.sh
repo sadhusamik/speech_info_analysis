bbb859399f0ca4218ddf0d35f876287fb7313bfb
samiksadhu <sadhusamik@gmail.com>
2022-06-22 11:53:59 -0400

    average spectrum computation

--- bin/compute_average_spectrum.sh
+++ bin/compute_average_spectrum.sh
@@ -27,12 +27,11 @@ feat_dir=`perl -e '($dir,$pwd)= @ARGV; if($dir!~m:^/:) { $dir = "$pwd/$dir";} pr
 mkdir -p $feat_dir
 
 name=`basename $data_dir`
-scp=$data_dir/wav.scp
 
-add_opts=""
+add_segment=false
 if [ -f "$data_dir/segments" ]; then
     echo "Adding segment files from $data_dir/segments"
-    add_opts="${add_opts} --segment_file=$data_dir/segments"
+    add_segment=true
 fi
 log_dir=$data_dir/log
 
@@ -40,33 +39,38 @@ mkdir -p $log_dir
 log_dir=`realpath ${log_dir}`
 
 
-# split files
+# split data dir
+echo "$0: Splitting data dir"
+split_data.sh ${data_dir} $nj || exit 1;
 
-echo "$0: Splitting scp files..."
-
-split_scp=""
-for n in $(seq $nj); do
-split_scp="$split_scp $log_dir/wav_${name}.$n.scp"
-done
-
-utils/split_scp.pl $scp $split_scp || exit 1;
 
 echo "$0: Computing average spectral features for scp files..."
 
 # Compute mel spectrum features
 
-$cmd --mem 5G JOB=1:$nj \
+if $add_segment; then
+  $cmd --mem 5G JOB=1:$nj \
   $log_dir/acc_spectrum_${name}_${speech_type}_${add_reverb}.JOB.log \
   ${exec_file} ${add_opts} \
-    $log_dir/wav_${name}.JOB.scp \
+    $data_dir/split50utt/JOB/wav.scp \
     $feat_dir/avg_spectrum_${name}.JOB.pkl \
+    --segment_file=$data_dir/split50utt/JOB/segments \
     --add_reverb=${add_reverb} \
     --fduration=${fduration} \
     --overlap_fraction=${overlap_fraction} \
     --speech_type=${speech_type} \
     --srate=16000 || exit 1;
-
-rm $log_dir/wav_${name}.*.scp
-
+else
+  $cmd --mem 5G JOB=1:$nj \
+    $log_dir/acc_spectrum_${name}_${speech_type}_${add_reverb}.JOB.log \
+    ${exec_file} ${add_opts} \
+      $data_dir/split50utt/JOB/wav.scp \
+      $feat_dir/avg_spectrum_${name}.JOB.pkl \
+      --add_reverb=${add_reverb} \
+      --fduration=${fduration} \
+      --overlap_fraction=${overlap_fraction} \
+      --speech_type=${speech_type} \
+      --srate=16000 || exit 1;
+fi
 
 echo $0": Finished computing average spectrum for $name"
