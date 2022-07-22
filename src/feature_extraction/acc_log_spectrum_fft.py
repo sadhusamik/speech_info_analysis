840ccab7a2be345244fd8285c56f63b7097eb0fd
samiksadhu <sadhusamik@gmail.com>
2022-07-22 14:59:03 -0400

    average spectrum computation

--- src/feature_extraction/acc_log_spectrum_fft.py
+++ src/feature_extraction/acc_log_spectrum_fft.py
@@ -72,8 +72,8 @@ def compute_modulations(args):
                     #        raise ValueError("speech_type can only be 'clean' or 'reverb'")
                     # else:
                     #    sig_out = signal
-                    signal = signal[0:16000 * 4]
-                cc, logmag, phase = feat_model.acc_log_spectrum_fft(signal, append_len=args.append_len)
+                #signal = signal[0:16000 * 4]
+                cc, logmag, phase = feat_model.acc_log_spectrum_fft(signal, append_len=args.append_len, discont=np.pi/2)
                 if cc is not None:
                     acc_logmag += logmag
                     acc_phase += phase
